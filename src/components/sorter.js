9087f4de61c94fb4cea09bc3fadb8257a74acf90
Vlada Zorina <vlada2227@mail.ru>
2020-05-04 13:14:02 +0300

    Ввели новые функции для отрисовки

--- src/components/sorter.js
+++ src/components/sorter.js
@@ -1,3 +1,5 @@
+import {createElement} from "../utils.js";
+
 export const createSorterTemplate = () => {
   return (
     `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
@@ -22,3 +24,25 @@ export const createSorterTemplate = () => {
           </form>`
   );
 };
+
+export default class Sort {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createSorterTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
   BEFOREEND: `beforeend`
 };
 
@@ -33,6 +34,9 @@ export const createElement = (template) => {
 };
 export const render = (container, element, place) => {
   switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
     case RenderPosition.BEFOREEND:
       container.append(element);
       break;
